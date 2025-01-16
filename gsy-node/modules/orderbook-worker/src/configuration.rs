9c436af394c1ba47632209195349ea10d4419767
spyrostz <spyros@gridsingularity.com>
2025-01-16 21:58:24 +0100

    Added trades transmission to the orderbook storage from the offchain worker.

--- gsy-node/modules/orderbook-worker/src/configuration.rs
+++ gsy-node/modules/orderbook-worker/src/configuration.rs
@@ -1,22 +1,23 @@
 use scale_info::prelude::string::String;
+use scale_info::prelude::format;
 use core::option_env;
 use codec::alloc::string::ToString;
 
 #[derive(Debug)]
-pub struct OrderBookServiceURL {
-	pub url: String,
+pub struct OrderBookServiceURLs {
+	pub orders_url: String,
+	pub trades_url: String
 }
 
-impl Default for OrderBookServiceURL{
+impl Default for OrderBookServiceURLs{
 	fn default() -> Self {
 		// Set the environment variable "URL" for OrderBook_Service
 		let orderbook_env_var = option_env!("ORDERBOOK_SERVICE_URL");
-		if orderbook_env_var.is_none() {
-			OrderBookServiceURL{ url: "http://localhost:8080/orders".to_string() }
-		}
-		else {
-			OrderBookServiceURL{ url: orderbook_env_var.unwrap().to_string() }
-		}
+		let orderbook_url = if orderbook_env_var.is_none() { "http://localhost:8080".to_string() } else { orderbook_env_var.unwrap().to_string() };
 
+		OrderBookServiceURLs{
+			orders_url: format!("{}/orders", orderbook_url),
+			trades_url: format!("{}/trades", orderbook_url),
+		}
 	}
 }
