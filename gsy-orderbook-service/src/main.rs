39678de97909ac1f3a97aa03f0a6a41e7e65a7e2
keyanhov <194363256+keyanhov@users.noreply.github.com>
2025-08-28 15:58:14 +0200

    fix: Correct order hashing in DB to match on-chain events

--- gsy-orderbook-service/src/main.rs
+++ gsy-orderbook-service/src/main.rs
@@ -10,37 +10,30 @@ use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    dotenv::dotenv().ok();
+	dotenv::dotenv().ok();
 
-    let subscriber = get_subscriber(
-        "gsy-orderbook-service".into(),
-        "info".into(),
-        std::io::stdout,
-    );
-    init_subscriber(subscriber);
-    let configuration = get_configuration().expect("Failed to load configuration");
-    let db_connection_string = configuration.get_connection_string();
-    let node_url = configuration.get_node_url();
-    let scheduler_interval = configuration.get_scheduler_interval();
-    let db_connection_wrapper =
-        init_database(db_connection_string, configuration.database_name).await?;
-    let db: DbRef = web::Data::new(db_connection_wrapper.clone());
-    let db_event_listener_instance = web::Data::clone(&db);
-    if !node_url.is_empty() {
-        tokio::task::spawn(async move {
-            let _ = init_event_listener(db_event_listener_instance, node_url).await;
-        });
-        tokio::task::spawn(async move {
-            start_scheduler(db, scheduler_interval).await;
-        });
-    }
-    let address = format!(
-        "{}:{}",
-        configuration.application_host, configuration.application_port
-    );
-    let listener = TcpListener::bind(address).expect("Failed to bind");
-    match run(listener, db_connection_wrapper)?.await {
-        Ok(_) => Ok(()),
-        Err(e) => Err(Error::from(e)),
-    }
+	let subscriber = get_subscriber("gsy-orderbook-service".into(), "info".into(), std::io::stdout);
+	init_subscriber(subscriber);
+	let configuration = get_configuration().expect("Failed to load configuration");
+	let db_connection_string = configuration.get_connection_string();
+	let node_url = configuration.get_node_url();
+	let scheduler_interval = configuration.get_scheduler_interval();
+	let db_connection_wrapper =
+		init_database(db_connection_string, configuration.database_name).await?;
+	let db: DbRef = web::Data::new(db_connection_wrapper.clone());
+	let db_event_listener_instance = web::Data::clone(&db);
+	if !node_url.is_empty() {
+		tokio::task::spawn(async move {
+			let _ = init_event_listener(db_event_listener_instance, node_url).await;
+		});
+		tokio::task::spawn(async move {
+			start_scheduler(db, scheduler_interval).await;
+		});
+	}
+	let address = format!("{}:{}", configuration.application_host, configuration.application_port);
+	let listener = TcpListener::bind(address).expect("Failed to bind");
+	match run(listener, db_connection_wrapper)?.await {
+		Ok(_) => Ok(()),
+		Err(e) => Err(Error::from(e)),
+	}
 }
