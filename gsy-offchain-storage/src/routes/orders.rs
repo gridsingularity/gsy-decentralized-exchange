61848cc605d35c5cd699d61f7591969d1195831d
hannesdiedrich <hannes@gridsingularity.com>
2026-08-12 14:49:58 +0200

    DD-406: Add new EWDS DTOs the rest endpoints

--- gsy-offchain-storage/src/routes/orders.rs
+++ gsy-offchain-storage/src/routes/orders.rs
@@ -3,17 +3,28 @@ use crate::routes::validate_start_end_time;
 use actix_web::{web::Json, web::Query, HttpResponse, Responder};
 use anyhow::{Error, Result};
 use primitives::db_api_schema::orders::{DbOrderSchema, FlexibilityOrderSchema};
+use primitives::ewds::dto::EwdsOrderDto;
 use serde::Deserialize;
 
-#[tracing::instrument(name = "Adding new orders", skip(db), fields(orders = ?orders))]
-pub async fn post_orders(orders: Json<Vec<DbOrderSchema>>, db: DbRef) -> impl Responder {
-    match db.get_ref().orders().insert_orders(orders.to_vec()).await {
+pub async fn post_orders(orders: Json<Vec<EwdsOrderDto>>, db: DbRef) -> impl Responder {
+    let db_orders: Result<Vec<DbOrderSchema>, _> = orders
+        .into_inner()
+        .into_iter()
+        .map(DbOrderSchema::try_from)
+        .collect();
+
+    let db_orders = match db_orders {
+        Ok(o) => o,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+
+    match db.get_ref().orders().insert_orders(db_orders).await {
         Ok(ids) => HttpResponse::Ok().json(ids),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
-pub async fn post_normalized_orders(orders: Json<Vec<DbOrderSchema>>, db: DbRef) -> impl Responder {
+pub async fn post_normalized_orders(orders: Json<Vec<EwdsOrderDto>>, db: DbRef) -> impl Responder {
     post_orders(orders, db).await
 }
 
@@ -30,12 +41,12 @@ pub struct OrdersParameters {
 async fn filter_orders_from_db(
     db: DbRef,
     orders_parameters: Query<OrdersParameters>,
-) -> Result<Vec<DbOrderSchema>, Error> {
-    if orders_parameters.market_id.is_none()
+) -> Result<Vec<EwdsOrderDto>, Error> {
+    let orders = if orders_parameters.market_id.is_none()
         && orders_parameters.start_time.is_none()
         && orders_parameters.end_time.is_none()
     {
-        db.get_ref().orders().get_all_orders().await
+        db.get_ref().orders().get_all_orders().await?
     } else {
         db.get_ref()
             .orders()
@@ -44,8 +55,9 @@ async fn filter_orders_from_db(
                 orders_parameters.start_time,
                 orders_parameters.end_time,
             )
-            .await
-    }
+            .await?
+    };
+    Ok(orders.into_iter().map(EwdsOrderDto::from).collect())
 }
 
 pub async fn get_orders(db: DbRef, orders_parameters: Query<OrdersParameters>) -> impl Responder {
@@ -56,7 +68,9 @@ pub async fn get_orders(db: DbRef, orders_parameters: Query<OrdersParameters>) -
     }
 
     match filter_orders_from_db(db, orders_parameters).await {
-        Ok(orders) => HttpResponse::Ok().json(orders),
+        Ok(orders) => HttpResponse::Ok().json(
+            orders.into_iter().map(EwdsOrderDto::from).collect::<Vec<_>>()
+        ),
         Err(e) => {
             tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
